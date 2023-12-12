56698f729a1afb16947b142092475253d32ea606
Suresh <sureshdankhara@gmail.com>
2023-12-12 18:48:52 +0530

    new ui implemented

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -37,17 +37,17 @@ const Results = () => {
     }
   }, [router.query.token]);
 
-  const improve = JSON.parse(data[0].improvements || '[]');
+  const improve = JSON.parse(data[0]?.improvements || '[]');
   const improve1 = improve[0];
 
   const parsedData = JSON.parse(improve1 || '[]');
 
-  const positives = JSON.parse(data[0].positives || '[]');
+  const positives = JSON.parse(data[0]?.positives || '[]');
   const parsedPositives = positives[0];
 
   const parsedPositivesData = JSON.parse(parsedPositives || '[]');
 
-  const rating = JSON.parse(data[0].rating || '[]');
+  const rating = JSON.parse(data[0]?.rating || '[]');
 
   const data2 = rating.map((item: { score: any }) => item.score);
 
@@ -61,28 +61,31 @@ const Results = () => {
   const averageOutOf10 = (average / 10) * 10;
 
   return (
-    <div className="container px-5 py-4  mx-auto">
+    <section className="container max-w-[90rem] mx-auto text-gray-600 body-font bg-gray-950">
       {isLoading && <Spinner />}
-      <div className="  grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2  gap-14 content-center">
-        {/* image section */}
-        <div>
-          <h1 className="text-white text-[50px] font-bold mb-5">
-            Dashboard Design
-          </h1>
-          <div className="pl-4">
-            {/* <div className="w-[679px] h-[398px]"> */}
-            <div className="w-[540px] h-[320px]">
-              <img
-                src={
-                  data[0].image
-                    ? `data:image;base64,${data[0].image}`
-                    : '/img/background.jpg'
-                }
-                alt="photo"
-                className="w-full h-full object-cover"
-              />
-            </div>
-            <h3 className="h-15 text-white text-[32px] font-extrabold my-5">
+
+      <div className="w-[90%] md:w-[60%] lg:w-[50%] h-auto mx-auto p-2 bg-rose-600 bg-opacity-10 rounded-lg border border-rose-600 my-7">
+        <p className="text-center text-white text-base font-normal leading-tight">
+          This page will be deleted once you leave this page - Create an account
+          to save feedback
+        </p>
+      </div>
+      <h1 className="w-[80%] md:w-[40%] lg:w-[37%] mx-auto text-transparent bg-gradient-to-r from-[#EBF1FF] to-[#B3C0DE] bg-clip-text text-[58px] md:text-[60px] font-bold leading-[76.80px]">
+        Name Dashboard
+      </h1>
+      {/* image section */}
+      <div className="w-[80%] h-[448px] mx-auto rounded-lg mt-16 mb-16">
+        <img
+          src={
+            data[0]?.image
+              ? `data:image;base64,${data[0]?.image}`
+              : '/img/background.jpg'
+          }
+          alt="photo"
+          className="w-full h-full object-cover rounded-lg"
+        />
+      </div>
+      {/* <h3 className="h-15 text-white text-[32px] font-extrabold my-5">
               Overall Score:{' '}
               <span className="text-lime-400 text-[34px]">
                 {Math.round(averageOutOf10) || 0}/10
@@ -93,8 +96,8 @@ const Results = () => {
                 className="bg-[#A1FC58] h-3.5 rounded-full"
                 style={{ width: Math.round(averageOutOf10) * 10 + '%' }}
               ></div>
-            </div>
-            <h3 className="h-15 text-white text-[32px] font-extrabold my-5">
+            </div> */}
+      {/* <h3 className="h-15 text-white text-[32px] font-extrabold my-5">
               Areas for Improvement:
             </h3>
 
@@ -107,113 +110,117 @@ const Results = () => {
                   {item.title}
                 </span>
               ))}
-            </div>
-          </div>
-        </div>
+            </div> */}
 
-        {/* Rating breakdown */}
-        <div>
-          <h3 className="text-white text-[32px] font-extrabold">
-            Rating Breakdown
-          </h3>
+      {/* Rating breakdown */}
+      <div>
+        <h3 className="w-[30%] text-transparent bg-gradient-to-r from-[#EBF1FF] to-[#B3C0DE] bg-clip-text text-[32px] font-extrabold mb-8">
+          Rating Breakdown
+        </h3>
+        <ul className="grid grid-cols-1 gap-6 md:grid-cols-2">
           {rating.map((item: any, i: any) => (
-            <div key={item.title} className="mb-5">
-              <div className="flex justify-between mb-2">
-                <div className="text-white text-lg font-medium">
-                  {item.title}
-                </div>
-                <div className="text-lime-200 text-lg font-normal">
-                  {item.score}/10
+            <li className="col-span-1 p-4 bg-neutral-900 rounded-2xl border border-gray-900 justify-start items-center gap-4 inline-flex">
+              <div className="p-2 bg-white bg-opacity-10 rounded-xl justify-start items-start gap-2 flex">
+                <div className="w-10 h-10 justify-center items-center flex">
+                  <div className="w-10 h-10 relative"></div>
                 </div>
               </div>
-              <div
-                className="w-full rounded-full h-3 dark:bg-gray-700 mb-2"
-                style={{
-                  backgroundColor:
-                    i == 0
-                      ? 'rgba(218, 126, 214, 0.259)'
-                      : i == 1
-                      ? 'rgba(230, 141, 76, 0.259)'
-                      : i == 2
-                      ? 'rgba(247, 249, 124, 0.259)'
-                      : i == 3
-                      ? 'rgba(121, 201, 236, 0.259)'
-                      : i == 4
-                      ? 'rgba(151, 236, 111, 0.259)'
-                      : '#000000',
-                }}
-              >
-                <div
-                  className="h-3 rounded-full "
-                  style={{
-                    width: Math.round(item.score) * 10 + '%',
-                    backgroundColor:
-                      i == 0
-                        ? '#da7ed6'
-                        : i == 1
-                        ? '#E68C4B'
-                        : i == 2
-                        ? '#F6F97C'
-                        : i == 3
-                        ? '#78C9EB'
-                        : i == 4
-                        ? '#96EC6E'
-                        : '#000000',
-                  }}
-                ></div>
+              <div className="grow shrink basis-0 flex-col justify-start items-start gap-4 inline-flex">
+                <div className="self-stretch justify-between items-center inline-block lg:inline-flex">
+                  <div className="grow shrink basis-0 text-white text-base font-medium">
+                    {item.title}
+                  </div>
+                  <div className="rounded justify-start items-center gap-2 flex">
+                    <div className="w-[142px] h-3 relative">
+                      <div className="w-[142px] h-3 left-0 top-0 absolute bg-gray-800 rounded" />
+                      <div
+                        className="h-3 left-0 top-0 absolute bg-emerald-600 rounded"
+                        style={{ width: Math.round(item.score) * 10 + '%' }}
+                      />
+                    </div>
+                    <div className="text-white text-base font-medium">
+                      {item.score}/10
+                    </div>
+                  </div>
+                </div>
+                <div className="self-stretch text-slate-200 text-base font-normal leading-7">
+                  {item.Description}
+                </div>
               </div>
-              <p className="text-zinc-100 text-base font-light">
-                {item.Description}
-              </p>
-            </div>
+            </li>
           ))}
-        </div>
+        </ul>
       </div>
 
       {/* Good Feedback */}
-      <div className="bg-gray-700 p-5">
-        <div className="ml-10">
-          <h3 className="text-lime-300 text-[32px] font-extrabold my-5">
-            Good Feedback
-          </h3>
-        </div>
+      <div className="w-full px-4 py-6 bg-white bg-opacity-5 rounded-2xl flex-col justify-start items-start gap-8 inline-flex mt-16">
+        <h3 className="w-[70%] md:w-[30%] text-transparent bg-gradient-to-r from-[#EBF1FF] to-[#B3C0DE] bg-clip-text text-[32px] font-extrabold">
+          Good Feedback
+        </h3>
+
         {parsedPositivesData.map((item: any, index: any) => (
-          <div key={index} className="ml-16 mb-10">
-            <h2 className="text-stone-50 text-[22px] font-medium">
-              {index + 1}. {item.title}
-            </h2>
-            <p className="text-zinc-100 text-lg font-light pl-5 mt-5">
-              {item.Description}
-            </p>
+          <div className="w-full h-auto p-4 rounded-2xl border border-gray-900 justify-start items-center gap-4 inline-flex">
+            <div className="w-[52px] h-[52px] p-2 bg-white bg-opacity-10 rounded-xl justify-center items-center gap-2 flex">
+              <div className="text-slate-300 text-xl font-normal leading-9">
+                {index + 1}
+              </div>
+            </div>
+            <div className="grow shrink basis-0 flex-col justify-start items-start gap-4 inline-flex">
+              <div className="self-stretch justify-between items-center inline-flex">
+                <div className="grow shrink basis-0 text-white text-lg font-bold">
+                  {item.title}
+                </div>
+              </div>
+              <div className="self-stretch text-slate-300 text-base font-normal leading-7">
+                {item.Description}
+              </div>
+            </div>
           </div>
         ))}
       </div>
 
       {/* Areas for improvement */}
-      <div className="mt-10 ml-10">
-        <h3 className="h-15 text-[#f9a9a9] text-[30px] font-bold my-5">
+      <div className="mt-16 pb-16">
+        <h3 className="w-[70%] md:w-[30%] text-transparent bg-gradient-to-r from-[#EBF1FF] to-[#B3C0DE] bg-clip-text text-[32px] font-extrabold mb-8">
           Areas for Improvement
         </h3>
-        {parsedData.map((item: any, index: any) => (
-          <div key={index} className="ml-5 mb-10">
-            <h2 className="text-stone-50 text-[22px] font-medium">
-              {index + 1}. {item.title}
-            </h2>
-            <p className="text-zinc-100 text-lg font-light pl-5 mt-5">
-              {item.Description}
-            </p>
-            <p className="pl-5 mt-5">
-              <span className="text-red-400 text-lg font-medium mr-3">
-                Possible Solution:
-              </span>
-              <span className="text-zinc-100 text-lg font-light">
-                {item.PossibleSolution}
-              </span>
-            </p>
-          </div>
-        ))}
+        <ul className="grid grid-cols-1 gap-6 md:grid-cols-2">
+          {parsedData.map((item: any, index: any) => (
+            <li
+              className={`col-span-1 p-4 bg-neutral-900 rounded-2xl border border-gray-900 justify-start items-center gap-4 inline-flex ${index === parsedData.length - 1 ? 'w-[204%]' : ''}`}
+            >
+              <div className="w-[52px] h-[52px] p-2 bg-white bg-opacity-10 rounded-xl justify-center items-center gap-2 flex">
+                <div className="text-slate-300 text-xl font-normal leading-9">
+                  {index + 1}
+                </div>
+              </div>
+              <div className="grow shrink basis-0 flex-col justify-start items-start gap-2 inline-flex">
+                <div className="self-stretch h-auto flex-col justify-start items-start flex">
+                  <div className="self-stretch justify-between items-center inline-flex">
+                    <div className="grow shrink basis-0 text-white text-lg font-bold">
+                      {item.title}
+                    </div>
+                  </div>
+                  <div className="self-stretch text-slate-300 text-base font-normal leading-7">
+                    {item.Description}
+                  </div>
+                </div>
+                <div className="self-stretch justify-between items-start inline-flex">
+                  <div className="grow shrink basis-0">
+                    <span className="text-emerald-600 text-base font-normal leading-7">
+                      Possible Solution:{' '}
+                    </span>
+                    <span className="text-slate-300 text-base font-normal leading-7">
+                      {item.PossibleSolution}
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
-    </div>
+    </section>
   );
 };
 
