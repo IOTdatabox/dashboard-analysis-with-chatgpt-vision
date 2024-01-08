2d4e57727b2363f12b41d71728d8aeadb8947512
data_box <whaydigital@gmail.com>
2024-01-08 12:06:59 -0800

    email validation again

--- src/components/Hero2.tsx
+++ src/components/Hero2.tsx
@@ -117,9 +117,9 @@ const Hero2 = () => {
   };
 
   const isValidEmail = (email: string) => {
-    const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    const regex = /^[a-zA-Z0-9._+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
     return regex.test(email);
-  };
+};
 
   // const tokenCount = () => {
   //   setCountToken(countToken + 1);
