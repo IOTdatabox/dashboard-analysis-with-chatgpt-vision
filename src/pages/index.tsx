4a053556ed5dbea8f8b91d1a00d867f576b7c05a
data_box <whaydigital@gmail.com>
2024-01-17 16:01:20 -0800

    build error fixed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,15 +3,10 @@ import Navbar from '@/components/Navbar';
 import Hero2 from '@/components/Hero2';
 import Teams from '@/components/Teams';
 import Footer from '@/components/Footer';
-import { Head } from 'next/document';
 
 export default function Home() {
   return (
     <>
-      {/* <Head>
-        <script src="https://player.vimeo.com/api/player.js"></script>
-
-      </Head> */}
       <Navbar />
       <Hero2 />
     </>
